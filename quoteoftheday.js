a9a4a6281d5cf9956c18f1dc1f54e6257e6a5d6c
IsaacB32 <isaaacbrowen@gmail.com>
2021-06-23 16:27:43 -0600

    Quote of the day to js and index

---
+++ quoteoftheday.js
@@ -0,0 +1,15 @@
+
+let quotes = 
+["Looking for something is a waste of time, losing something, now that's an art form.",
+"","",""];
+
+function UpdateText()
+{
+    alert("open");
+    document.getElementById("dayText").innerHTML = "testing";
+}
+
+function GetQuote()
+{
+    return quotes[0];
+}
