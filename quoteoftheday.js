e30d842eae54c1a4bae734398184a1d99fbd1489
IsaacB32 <isaaacbrowen@gmail.com>
2021-06-24 09:08:10 -0600

    Added quotes

--- quoteoftheday.js
+++ quoteoftheday.js
@@ -1,15 +1,18 @@
 
 let quotes = 
 ["Looking for something is a waste of time, losing something, now that's an art form.",
-"","",""];
+"An open house is like a garage sale for criminals",
+"Creating a website is a lot like raising children, it takes forever and they are ungrateful",""];
 
 function UpdateText()
 {
-    alert("open");
-    document.getElementById("dayText").innerHTML = "testing";
+    document.getElementById("dayText").innerHTML = GetQuote();
 }
 
 function GetQuote()
 {
-    return quotes[0];
+    var today = new Date();
+    var date = today.getDate();
+
+    return quotes[Math.min(quotes.length - 1, date)];
 }
