9ccd7a73eb96a5f8ba4029aad84ca8d04ac67a2b
Creedon Hood (personal) <creedonhood@gmail.com>
2021-06-24 16:10:55 -0600

    revision 1

---
+++ assets/js/parallax.js
@@ -0,0 +1,8 @@
+document.addEventListener("mousemove", parallax)
+const elem = document
+
+function parallax(e) {
+    const x = (window.innerWidth - e.clientX*2)/90 - 100
+    const y = (window.innerHeight - e.clientY*2)/90 - 100
+    document.getElementById("spaceimage").style.transform = `translateX(${x}px) translateY(${y}px)`
+}
