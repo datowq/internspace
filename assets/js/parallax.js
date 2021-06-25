0473c15f319ba07d3beb35d2540d1cde7899f2cc
datowq <steven.andrew.lew@gmail.com>
2021-06-25 10:41:35 -0600

    sasasxxs

--- assets/js/parallax.js
+++ assets/js/parallax.js
@@ -1,7 +1,7 @@
 document.addEventListener("mousemove", parallax)
 
 function parallax(e) {
-    const x = (window.innerWidth - e.clientX*-0.5)/90 -134
-    const y = (window.innerHeight - e.clientY*-0.5)/90 -137
+    const x = (window.innerWidth - e.clientX*-0.5)/90 - (window.innerWidth/14.5)
+    const y = (window.innerHeight - e.clientY*-0.5)/90 - (window.innerHeight/7.9)
     document.getElementById("spaceimage").style.transform = `translateX(${x}px) translateY(${y}px)`
 }
