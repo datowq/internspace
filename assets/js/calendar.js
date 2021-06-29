7de07b6163cea035aa90f87e42302d64d21b1844
Creedon Hood (personal) <creedonhood@gmail.com>
2021-06-29 14:46:09 -0600

    js not c#

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -1,40 +1,34 @@
-const daysofweek = ["Mo","Tu","We","Th","Fr","Sa","Su"]
+const daysofweek = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"]
 const startDateForMonth = [1]
 let monthStartIndex = 0
 
-function DrawCalendar()
-{
-    let menu = document.getElementById('calendar_week')
+function DrawCalendar() {
+	let menu = document.getElementById("calendar_week")
 
-    //draw days of the week
-    for(let i = 0; i < daysofweek.length; i++)
-    {
-        let li = document.createElement('li')
-        li.textContent = daysofweek[i]
-        menu.appendChild(li)
-    }
+	//draw days of the week
+	for (let i = 0; i < daysofweek.length; i++) {
+		let li = document.createElement("li")
+		li.textContent = daysofweek[i]
+		menu.appendChild(li)
+	}
 
-    menu = document.getElementById('calendar_day')
+	menu = document.getElementById("calendar_day")
 
-    //draw days of the week
-    for(let i = startDateForMonth[monthStartIndex] * -1; i <= 30; i++)
-    {
-        if(i == 0) i++
+	//draw days of the week
+	for (let i = startDateForMonth[monthStartIndex] * -1; i <= 30; i++) {
+		if (i == 0) i++
 
-        if(i > 0)
-        {
-            let li = document.createElement('li')
-            li.textContent = i
-            
-            menu.appendChild(li)
-        }
-        else
-        {
-            let space = document.createElement('li')
-            space.innerHTML = ''
-            menu.appendChild(space)
-        }
-    }
+		if (i > 0) {
+			let li = document.createElement("li")
+			li.textContent = i
+
+			menu.appendChild(li)
+		} else {
+			let space = document.createElement("li")
+			space.innerHTML = ""
+			menu.appendChild(space)
+		}
+	}
 }
 
 DrawCalendar()
